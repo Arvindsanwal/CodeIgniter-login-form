a003e7bcbfa220ab5af54af38e05f81c521b1540
arvind <arvind.kumar@sodelsolutions.com>
2013-06-17 16:21:36 +0530

    comlete login form

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,86 +1,33 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Login extends CI_Controller {
-
-	public function __construct()
-       {
-       	parent::__construct();
-          $this->load->helper('url'); 
-       }
-
-	/* function to load login page */
-	public function index()
-	{
-			
-		
-		$data['css']        = $this->config->item('css');  
-		$this->load->helper(array('form', 'url'));
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('email', 'Email', 'required|min_length[5]');
-		$this->form_validation->set_rules('password', 'Password', 'required');
-		if ($this->form_validation->run() == FALSE)
-		{
-			$this->load->view('login/Login',$data);
-			
-		}
-		else
-		{
-			$this->checkUser();
-		}
-		
-		
-	}
-
-	/* function to function to check user details */
-	public function checkUser()
-	{
-		$this->load->model('login/checkuser_model');
-		$result = $this->checkuser_model->checkUser();
-		if($result != "Not Available")
-		{
-			$fname =$result->first_name;
-			$lname =$result->last_name;
-			$data = array("fname" => $fname, "lname" => $lname);
-			$this->load->view('login/Success',$data);
-		}
-		else
-		{
-			echo "Wrong User name or Password";
-			//$this->index();
-		}
-
-	}
-
-	/* function for forgot password */
-	public function forgotPassword()
-	{
-		$data['css']        = $this->config->item('css');  
-		$this->load->view('login/ForgotPassword',$data);
-	}
-
-	/*function to load registration form*/
-	public function registerationForm()
-	{
-		$data['base']       = $this->config->item('base_url');
-	    $data['css']        = $this->config->item('css');  
-		$this->load->view('login/Register',$data);
-	}
-
-	public function registeration()
-	{
-		$this->load->model('login/registeration_model');
-		$result = $this->registeration_model->register();
-		if($result != "Not Available")
-		{
-			$fname =$result->first_name;
-			$lname =$result->last_name;
-			$data = array("fname" => $fname, "lname" => $lname);
-			$this->load->view('login/Registration_success',$data);
-		}
-	}
-
-
-
+class Login extends CI_Controller{
+    
+    function __construct(){
+        parent::__construct();
+    }
+    
+    public function index($msg = NULL){
+        // Load our view to be displayed
+        // to the user
+        $data['msg'] = $msg;
+        $this->load->view('login_view', $data);
+    }
+    
+    public function process(){
+        // Load the model
+        $this->load->model('login_model');
+        // Validate the user can login
+        $result = $this->login_model->validate();
+        // Now we verify the result
+        if(! $result){
+            // If user did not validate, then show them login page again
+            $msg = '<font color=red>Invalid username and/or password.</font><br />';
+            $this->index($msg);
+        }else{
+            // If user did validate,
+            // Send them to members area
+            redirect('home');
+        }        
+    }
 }
+?>
