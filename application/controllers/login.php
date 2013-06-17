19850c517bb22f8b92dd0306f847158fe3c407a9
arvind <arvind.kumar@sodelsolutions.com>
2013-06-17 15:31:28 +0530

    login file

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -23,33 +23,7 @@ class Login extends CI_Controller {
 		if ($this->form_validation->run() == FALSE)
 		{
 			$this->load->view('login/Login',$data);
-			/*Start Mail code*/
-			/*		$config = array(
-							  'protocol' => 'smtp',
-							  'smtp_host' => 'ssl://smtp.googlemail.com',
-							  'smtp_port' => 465,
-							  'smtp_user' => 'ak773399@gmail.com',
-							  'smtp_pass' => '9466324811',
-							);
-
-			$this->load->library('email', $config);
-			$this->email->set_newline("\r\n");
-
-			$this->email->from('arvind.kumar@sodelsolutions.com', 'Mr. Test');
-			$this->email->to('surajk@sodelsolutions.com');
-
-			$this->email->subject(' My mail through codeigniter from localhost ');
-			$this->email->message('Hello World…');
-
-			if (!$this->email->send())
-			show_error($this->email->print_debugger());
-			else
-			echo 'Your e-mail has been sent!';*/
-
-			/*End Mail code*/
 			
-
-
 		}
 		else
 		{
